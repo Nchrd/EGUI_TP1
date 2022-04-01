bef60cdd3682f7796638b0fbeae55758b24014e9
Noé Echard <65755297+Nchrd@users.noreply.github.com>
2022-04-01 15:44:51 +0200

    Added QSS File, change in interface

--- Qt_blog/main.cpp
+++ Qt_blog/main.cpp
@@ -3,10 +3,20 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleSheetFile(":/rec/styleSheets/SyNet.qss");
+
+    if(!styleSheetFile.exists()) qDebug() << "File does not exist";
+
+    styleSheetFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet(styleSheet);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
